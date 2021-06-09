b9a1c5dfb7137eb66fc83935c2152f39dcc6f28e
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2021-06-09 16:50:09 -0400

    db createProductListings

---
+++ server/config/keys.js
@@ -0,0 +1,9 @@
+const { Collection } = require("mongoose");
+
+module.exports = {
+    mongoURI: "mongodb+srv://w:q@cluster0.lkscp.mongodb.net/DB?retryWrites=true&w=majority",
+    DB:"DB",
+    Collections:{
+        ProductsPriceListings:"ProductsPriceListings"
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,42 @@
-
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb');
 
+//Bodyparser Middleware
+// const app = express();
+// app.use(express.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+const keys = require('./config/keys');
+//Connect to Mongo
 
 async function main() {
-    const uri = "mongodb+srv://w:q@cluster0.lkscp.mongodb.net/DB?retryWrites=true&w=majority";
-
-    const client = new MongoClient(uri, {useUnifiedTopology:true});
+    
+    const client = new MongoClient(db, {useUnifiedTopology:true});
 
     try {
         await client.connect();
         
-        createListings(client, {
-            
-        })
+        // await createListing(client, {
+        //     name: "DP ENVY",
+        //     price: "799"
+        // })
+        await createMultipleListings(client, [
+            {
+                name: "CP ENVY",
+                price: "799"
+            },
+            {
+                name: "BP ENVY",
+                price: "799"
+            },
+            {
+                name: "AP ENVY",
+                price: "799"
+            },
+        ]);
 
     } catch(e) {
         console.error(e);
@@ -20,17 +44,29 @@ async function main() {
         await client.close();
     }
 
-
 }
 
 main().catch(console.error);
 
-async function createListings(client, newListing) {
-    const result = await client.db('DB').collection("listingsAndReviews").insertOne
-    (newListing);
+//CRUD - create
+async function createMultipleListings(client, newListings) {
+    const result = await client.db(keys.DB).collection(keys.Collections.ProductsPriceListings)
+    .insertMany(newListings);
+    console.log(`${result.insertedCount} new Listings created with the following id(s):`);
+    console.log(result.insertedIds);
+}
 
-    console.log(`new id:`)
+async function createListing(client, newListing) {
+    const result = await client.db('DB').collection("ProductsPirceListings")
+    .insertOne(newListing);
 
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+
+}
+
+//CRUD - read
+async function findOneListingByName(client, nameOfListing) {
+    // const result = await client.db(DB).collection("ProductsListings")
 }
 
 async function listDatabases(client) {
