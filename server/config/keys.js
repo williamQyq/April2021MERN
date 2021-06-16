7f1f9b7206b78feef00655781a15487f1f23ad93
Yuqing Qiao <39916920+williamQyq@users.noreply.github.com>
2021-06-16 18:02:45 -0400

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-const { Collection } = require("mongoose");
-
-module.exports = {
-    mongoURI: "mongodb+srv://w:q@cluster0.lkscp.mongodb.net/DB?retryWrites=true&w=majority",
-    DB:"DB",
-    Collections:{
-        ProductsPriceListings:"product_price_listings"
-    }
-};
