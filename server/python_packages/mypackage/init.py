37b60254559f2129c39d6f864fac8a023e152579
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2021-08-26 17:26:06 -0400

    deploy test

--- server/python_packages/mypackage/init.py
+++ server/python_packages/mypackage/init.py
@@ -10,10 +10,10 @@ def init_chrome_driver():
     chrome_options = webdriver.ChromeOptions() 
 
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
     # chrome_options.add_experimental_option("detach",True)                                   #Run Chrome driver without closing when finished
--- server/python_packages/priceTracker.py
+++ server/python_packages/priceTracker.py
@@ -7,7 +7,7 @@ from mypackage.fun import track_instock_info
 def main():
 
     # retrieve py_process argument product_price_list JSON string to array of dictionary
-    # product_link_list = json.loads(sys.argv[1])
+    product_link_list = json.loads(sys.argv[1])
     
     # product_link_list = [{
     #     "_id": "611431d814c6b148f0fc0bb4", 
@@ -20,15 +20,14 @@ def main():
     #     }
     #     }]
 
-    # driver = init_chrome_driver()                           # init chrome driver for selenium
+    driver = init_chrome_driver()                           # init chrome driver for selenium
+    
+    track_instock_info(product_link_list, driver)           # modify mutable list of dictionay: product_link_list
 
-    # track_instock_info(product_link_list, driver)           # modify mutable list of dictionay: product_link_list
+    jsonString = json.dumps(product_link_list)              # convert dic to json string then print
+    print(jsonString)
 
-    # jsonString = json.dumps(product_link_list)              # convert dic to json string then print
-    # print(jsonString)
-
-    # driver.quit()
-    # sys.stdout.flush()
-    print("hello python child process")
+    driver.quit()
+    sys.stdout.flush()
 
 main()
