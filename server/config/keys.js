cbb0e15c01d94199ad0ee08afd5807fc3734b208
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2021-08-28 13:42:09 -0400

    .

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-const { Collection } = require("mongoose");
-
-module.exports = {
-    mongoURI: "",
-    DB:"DB",
-    Collections:{
-        ProductsPriceListings:"product_price_listings"
-    }
-};
