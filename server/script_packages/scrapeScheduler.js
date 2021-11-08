b2239b0ac2045617fd9092cd951ba14a07eb30c2
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2021-11-08 12:05:12 -0500

    random timer

--- server/script_packages/scrapeScheduler.js
+++ server/script_packages/scrapeScheduler.js
@@ -2,20 +2,33 @@ const cron = require('node-cron');
 const { pyProcessBB } = require('./scraper.js');
 
 //cron scheduler run pyProcessBB get bb prices at 6 pm everyday
-const scrapeBBScheduler = cron.schedule("00 00 10 * * *", () => {
-    console.log("\n[Scheduler bb] starting to get bb prices...")
-    
-    setTimeout(() => {
-        pyProcessBB()
-    }, 5000)
-
-    console.log('[Scheduler bb] job completed.')
+const scrapeBBScheduler = cron.schedule("00 00 09 * * *", () => {
+    randomDelayScraper();
 
 });
 
-const getRandom2DigitString = (max) => {
-    let num = Math.floor(Math.random() * max);
-    return (num).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+const randomDelayScraper = () => {
+    let count = 0;
+    let interval = setInterval(() => {
+        console.log(`${(count + 1)} minutes pass...`);
+        count += 1;
+    }, 60000);
+
+    //run scraper in an hour, random time.
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log(`Delay timer finished.`)
+
+        pyProcessBB();
+
+    }, getRandomMins(60))
+}
+// get random mins less than max
+const getRandomMins = (max) => {
+    min = Math.floor(Math.random() * max);
+    sec = min * 60000;
+    console.log(`Random delay mins: ${min}`);
+    return sec;
 }
 
 module.exports = {
