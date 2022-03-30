3c337a4de9440e3f3a88f5d7c0c4cae34efb3d89
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2022-03-30 14:33:30 -0400

    reverse email filter space

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -12,19 +12,17 @@ const router = express.Router();
 // @desc:   authorize users login
 router.post('/', (req, res) => {
     const { email, password } = req.body;
-    let validateEmail = email.replace(/\s/g, '');
-    let validatePassword = password.replace(/\s/g, '');
 
-    if (!validateEmail || !validatePassword) {
+    if (!email || !password) {
         return res.status(400).json({ msg: 'All fields required' });
     }
 
-    User.findOne({ validateEmail })
+    User.findOne({ email })
         .then(user => {
             if (!user) return res.status(400).json({ msg: 'Unauthorized access denied' });
 
             //validate password
-            bcrypt.compare(validatePassword, user.password)
+            bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (!isMatch) return res.status(400).json({ msg: 'Unauthorized credentials' });
 
