b1083957b484d2a0c752f282784441ee8b74358a
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2022-06-30 12:44:22 -0400

    leaky bucket typescript

--- server/bin/amazonSP/RateLimiter.ts
+++ server/bin/amazonSP/RateLimiter.ts
@@ -3,12 +3,27 @@ import { performance } from 'perf_hooks';
 type T = any;
 type Time = number;
 type Task = () => Promise<T>;
-type RequestLimit = T;
+type RequestLimit = {
+    ratePerSec: number,
+    burst?: number
+};
+type TaskResponse = T;
 
 interface LeakyBucket {
-    readonly capacity: number,
-    performance: Time,
-    queue: Task[]
+    readonly capacity: number;
+    performance: Time;
+    queue: Task[];
+
+    start(): Promise<Array<TaskResponse>>;
+    isQueueEmpty(): boolean;
+
+    _enqueue(task: Task): void;
+    _dequeue(): void;
+    _delay(ms: number): Promise<void>;
+    _measureTaskHandlingPerformanceAndResovle(tasks: Task[]): Promise<Array<TaskResponse>>;
+    _delayOnLimitReached(index: number, limit: RequestLimit, cost: Time): Promise<void>;
+    _clearTimer(): void;
+
 }
 
 class LeakyBucket {
@@ -19,6 +34,21 @@ class LeakyBucket {
 
     }
 
+    async start() {
+        let isQueueEmpty = this.isQueueEmpty();
+        if (isQueueEmpty) {
+            return;
+        }
+        let tasks: Array<Task> = this.queue.map(task => task); //copy task queue.
+        this._clearQueue();
+
+        let results = await this._measureTaskHandlingPerformanceAndResovle(tasks);
+        return results;
+    }
+    isQueueEmpty() {
+        return this.queue.length > 0 ? false : true;
+    }
+
     _enqueue(task: Task): void {
         this.queue.push(task);
     }
@@ -26,27 +56,47 @@ class LeakyBucket {
     _dequeue(): void {
         this.queue.shift();
     }
+    _clearQueue(): void {
+        this.queue = new Array();
+    }
 
-    _delay(ms: number): Promise<void> {
+    async _delay(ms: number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
 
     async _measureTaskHandlingPerformanceAndResovle(tasks: Task[]) {
-        for (const task of tasks) {
+        let results = new Array();
+
+        for (const [index, task] of tasks.entries()) {
             let start = performance.now();
             let onPromiseDone = () => performance.now() - start;
             let taskResponse = await task();    //execute task,retrieve promise
+            results.push(taskResponse);
             let timeCost = onPromiseDone();
             if (taskResponse.limit !== undefined) {
-                await this._delayOnLimitReached(taskResponse.limit, timeCost)
+                await this._delayOnLimitReached(index, taskResponse.limit, timeCost)
             }
 
         }
-
+        return results;
     }
 
-    async _delayOnLimitReached(limit: RequestLimit, cost: Time) {
+    async _delayOnLimitReached(index: number, limit: RequestLimit, cost: Time) {
+        this._performance += cost;
+        let rpc = limit.ratePerSec;
+        let isRpcReached = (index + 1) % rpc == 0 ? true : false;
 
+        if (isRpcReached) {
+            if (this._performance >= 1000) {
+                console.log(`Req Rate Limit Reached, delay 1 sec...`)
+                await this._delay(1000);
+            }
+            this._clearTimer();
+        }
+    }
+
+    _clearTimer() {
+        this._performance = 0;
     }
 }
 
